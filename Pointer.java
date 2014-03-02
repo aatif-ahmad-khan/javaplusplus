2e658e8604a23b6fe6ba2483d0176a8970efeb9d
aatif-ahmad-khan <aatif-ahmad-khan@users.noreply.github.com>
2014-03-02 23:13:37 +0530

    Create Pointer.java
    This is Pointer class of AM.Aatif.* package having only one member 'address'. 'address' will store the memory address of the variable to which Pointer is pointing to.

---
+++ Pointer.java
@@ -0,0 +1,20 @@
+/**
+* This is Pointer class of AM.Aatif.* package.
+*/
+
+package AM.Aatif;
+
+/**
+* @author Aatif Ahmad Khan
+*/
+
+public class Pointer {
+  
+  /**
+  * Member 'address' will store the memory address of variable to
+  * which the Pointer is pointing to.
+  */
+  
+  public static long address;
+  
+}
